de820b166ceb90e06604fe4adde453ebadf9e920
JorgeCH <el.chicharo.magico@gmail.com>
2015-06-29 12:16:53 -0500

    js & delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy", as: "logout"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
   get '/internals/:id/print', to: 'internals#print'
   get '/valuations/:id/print', to: 'valuations#print'
